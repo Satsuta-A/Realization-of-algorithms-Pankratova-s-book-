4b63b383e0b3f914e1a1ff0602624524b7afb73c
Satsuta-A <tolya.satzuta@gmail.com>
2021-11-02 21:33:07 +0200

    Задание

--- Barrett 's reduction of numbers modulo.py
+++ Barrett 's reduction of numbers modulo.py
@@ -5,28 +5,29 @@ def Barretts_modulo(x, m, b = 10):
     x_list.reverse()
     m_list.reverse()
     n = len(m_str)
+    if len(x_str) > 2 * len(m_str) or b<=3 or m_list[n-1] == 0:
+        return None
     z = int(pov(b, 2 * n) / m)
 
     q = int(int(x / pov(b, n - 1)) * z / pov(b, n + 1)) #Шаг 1
     r1, r2 = x % pov(b, n + 1), (q * m) % pov(b, n + 1)#Шаг 2
 
-    if r1 > r2: #Шаг 3
+    if r1 >= r2: #Шаг 3
         r = r1 - r2
     else:
         r = pov(b, n + 1) + r1 - r2
-    while r > m: #Шаг 4
+    while r >= m: #Шаг 4
         r -= m
 
     return r #Шаг 5
 
 def test():
-    for i in range(11, 10000):
+    for i in range(12, 10000):
         for j in range(i-10, i):
-            if Barretts_modulo(i, j) == i % j:
-                pass
-            else:
-                print(f'Error! x = {i}, m = {j}')
+            if not Barretts_modulo(i, j) == i % j:
+                print(f'Error! x = {i}, m = {j}, r = {Barretts_modulo(i, j)}')
                 exit
+    print('Тесты успешно пройден!')
 
 if __name__ == "__main__":
     x = int(input('Введите число x: '))
@@ -35,4 +36,5 @@ if __name__ == "__main__":
 
     print('Ответ: ', Barretts_modulo(x, m))
     print('Должно быть: ', x % m)
+
     test()
---
+++ Reduction to a module of a special form.py
@@ -0,0 +1,39 @@
+from Exponentiation import Exponentiation as pov
+def Spec_form(x, m, b = 10):
+    bN,i = 1, 1
+    while bN*b <= m:
+        bN *= b
+        i += 1
+    bN = pov(b, i)
+    c = bN - m
+
+    r = x % bN
+    q = (x - r) // bN
+
+    #print(f'q = {q}, r = {r}, bN = {bN}, c = {c}')
+
+    while q > 0:
+        qc = q * c
+        rN = qc % bN
+        qN = (qc - rN) // bN
+        r, q = r + rN, qN
+    while r >= m:
+        r -= m
+    return r
+
+def test():
+    for i in range(11, 10000):
+        for j in range(i-10, i):
+            if not Spec_form(i, j) == i % j:
+                print(f'Error! x = {i}, m = {j}')
+                exit
+    print('Тесты успешно пройден!')
+
+if __name__ == "__main__":
+    x = int(input('Введите число x: '))
+    m = int(input('Введите число m: '))
+    #b = int(input('Введите число b: '))
+
+    print('Ответ: ', Spec_form(x, m))
+    print('Должно быть: ', x % m)
+    test()
