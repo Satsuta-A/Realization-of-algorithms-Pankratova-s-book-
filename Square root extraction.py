cecf471443d07c9c5e6535a3be26fb8836c550f0
Anatoly <57151681+Satsuta-A@users.noreply.github.com>
2021-11-03 22:00:42 +0200

    Update Square root extraction.py

--- Square root extraction.py
+++ Square root extraction.py
@@ -2,20 +2,13 @@ from common_func import BadNumberError as BNE
 def Square_root_extraction(a):
     if a < 0:
         raise BNE('Больше нуля!')
-    len_a = len(str(a))
-    if a >= 10:
-        for x in range(pow(10, len_a // 2 - 1), pow(10, len_a // 2 + 1)):
-            if x**2 > a:
-                break
-    else:
-        for x in range(pow(10, len_a // 2 + 1)):
-            if x**2 > a:
-                break
-
-    x0 = x - 1
+    x = a    
+    
+    x0 = x
+    
     while x < x0:
         x = (a // x + x) // 2
-    return x0
+    return int(x0 ** (0.5))
 
 def test():
     for x in range(6391):
