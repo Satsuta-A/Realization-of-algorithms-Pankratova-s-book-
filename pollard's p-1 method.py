1f550ce7bf58732abd2b343c00b6ffb99685f3f8
Satsuta-A <tolya.satzuta@gmail.com>
2022-01-21 22:45:36 +0200

    Задание

---
+++ pollard's p-1 method.py
@@ -0,0 +1,41 @@
+import math
+
+from common_func import gcd
+from random import randint
+from math import log
+from sympy import isprime
+def help_B(n):
+    i = -1
+    while n > 0:
+        n //= 10
+        i += 1
+    return i
+
+def p_1_pollard(n):
+    #B = int(input(f'n ~ {help_B(n)}\n'))
+    B = 20
+
+    a = randint(2, n-1)
+    d = gcd(a, n)
+    if d == 2:
+        return d
+
+    for q in range(B+1):
+        if isprime(q):
+            l = math.floor(math.log(110) // math.log(11))
+            a = pow(a, pow(q, l), n)
+
+    d = gcd(a - 1, n)
+
+    if d == 1 or d == n:
+        return 'FAIL'
+    else:
+        return d
+
+
+
+
+
+if __name__ == "__main__":
+    for x in range(100000, 200000):
+        print(p_1_pollard(x))
---
+++ pollard's p-method.py
@@ -0,0 +1,29 @@
+import math
+
+from common_func import gcd
+
+def p_method(n):
+    a, b = 2, 2
+    while True:
+        a = (pow(a, 2, n) + 1) % n
+        b = (pow(b, 2, n) + 1) % n
+        b = (pow(b, 2, n) + 1) % n
+
+        d = gcd(a * b, n)
+
+        if 1 < d and d < n:
+            return d
+        if d == n or d == 1:
+            return 'FAIL'
+
+if __name__ == "__main__":
+    fails = 0
+    succ = 0
+    for x in range(1, 122):
+        print(f'{x} divide by', p_method(x))
+        if p_method(x) == 'FAIL':
+            fails += 1
+        else:
+            succ += 1
+
+    print(f'fails = {fails}, succsess = {succ}')
